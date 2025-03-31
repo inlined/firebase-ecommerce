77bb465b95b3e35aff28212e0d91b02f8c652e71
Thomas Bouldin <inlined@google.com>
2025-03-31 11:39:59 -0700

    Remove debug print

--- site/src/lib/firebase/index.ts
+++ site/src/lib/firebase/index.ts
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 // Initialize Firebase
-console.log("Process.env is", JSON.stringify(process.env, null, 2));
 const app = initializeApp(JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG!));
 
 // Initialize Firebase Auth and export it
